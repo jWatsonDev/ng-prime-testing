0f509ffafc7a15e707bacc04b4f8a70da5c2ad51
Jay Watson <watson.jay@outlook.com>
2018-11-20 06:19:47 -0600

    Add toggle columns to bottom of table

--- src/app/prime/prime.component.ts
+++ src/app/prime/prime.component.ts
@@ -7,10 +7,10 @@ import { DataService } from '../data.service';
   styleUrls: ['./prime.component.css']
 })
 export class PrimeComponent implements OnInit {
-  public _data: any[];
+  public data: any[];
   public cols: any[];
-  public _sortField: string;
-  public _sortOrder: number;
+  public sortField: string;
+  public sortOrder: number;
   public selectedColumns: any[];
 
   constructor(public _dataService: DataService) { }
@@ -20,15 +20,15 @@ export class PrimeComponent implements OnInit {
   }
 
   onSortNotify(emission: number, sortBy: string) {
-    this._sortOrder = emission;
-    this._sortField = sortBy;
+    this.sortOrder = emission;
+    this.sortField = sortBy;
   }
 
   ngOnInit() {
     this._dataService.getData()
     .subscribe(data => {
-        this._data = data;
-        // console.log(data);
+      this.data = data;
+      // console.log(data);
     }, error => console.log(error));
 
     this.cols = [
@@ -37,10 +37,8 @@ export class PrimeComponent implements OnInit {
       { field: 'body', header: 'body', display: true }
     ];
 
-    this._sortField = 'id';
-    this._sortOrder = 1;
+    this.sortField = 'id';
+    this.sortOrder = 1;
     this.selectedColumns = this.cols;
   }
-
-
 }
