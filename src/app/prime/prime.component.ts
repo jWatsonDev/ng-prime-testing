4de43cf7d9991d2bac9ee5a65158b979aa3f423f
Watson <watsonjamd@corp.saic.com>
2018-11-21 14:44:10 -0600

    Add pdf print prototype

--- src/app/prime/prime.component.ts
+++ src/app/prime/prime.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
 
+declare var jsPDF: any;
+
 @Component({
   selector: 'app-prime',
   templateUrl: './prime.component.html',
@@ -25,6 +27,46 @@ export class PrimeComponent implements OnInit {
     this.sortField = sortBy;
   }
 
+ generatePdf() {
+    var columns = ["ID", "name", "blah"];
+    var rows = [];
+
+    for (const key of Object.keys(this.data)) {
+      rows.push(this.jsonToArray(this.data[key]));
+    }
+    console.log(rows);
+    var doc = new jsPDF();
+    doc.autoTable(columns, rows);
+    doc.save('Test.pdf');
+}
+
+  jsonToArray(json){
+    let result = [];
+    let keys = Object.keys(json);
+    keys.forEach(function(key){
+        result.push(json[key]);
+    });
+    return result.splice(1, result.length - 1);
+  }
+
+  printToPdf() {
+
+    //let doc = new jsPDF();
+    let col = ["Details", "Values", "dsflkjdfs"];
+    let rows = [];
+    console.log(JSON.stringify(this.data))
+    // for(let key in this.data){
+    //     let temp = [key, this.data[key]];
+    //     rows.push(temp);
+    // }
+    for (const key of Object.keys(this.data)) {
+      //console.log(this.data[key]);
+      rows.push(this.data[key]);
+    }
+    console.log(rows);
+    // doc.autoTable(col, rows);
+    // doc.save('Test.pdf');
+  }
   ngOnInit() {
     this.dataService.getData()
       .subscribe(data => {
