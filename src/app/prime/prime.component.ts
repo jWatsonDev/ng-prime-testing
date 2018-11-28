b92adb63eac7884bf7e6fca7bdea1b885c354cfe
Watson <watsonjamd@corp.saic.com>
2018-11-28 14:34:35 -0600

    Add selected data featured to pdf report

--- src/app/prime/prime.component.ts
+++ src/app/prime/prime.component.ts
@@ -28,19 +28,60 @@ export class PrimeComponent implements OnInit {
     this.sortField = sortBy;
   }
 
+  test() {
+    console.log(this.selectedColumns);
+    console.log(this.data);
+  }
+
+  generatePdf() {
+    let rows = [];
+    let originalCols = [];
+    let selectedCols = [];
+    let colsToDisplay = [];
+    let colsToDelete = [];
+
+    for (let key of Object.keys(this.cols)) { originalCols.push(this.cols[key].field) }
+
+    for (let key of Object.keys(this.selectedColumns)) {
+      colsToDisplay.push(this.selectedColumns[key].header);
+      selectedCols.push(this.selectedColumns[key].field);
+    }
+
+    for (let i = 0; i < originalCols.length; i++) {
+      console.log(originalCols[i]);
+      if (selectedCols.indexOf(originalCols[i]) === -1) {
+        colsToDelete.push(originalCols[i]);
+      }
+    }
+
+    //this.updateSelectedItems(this.data, 'blah');
+    for (let key of Object.keys(this.data)) {
+      let tempRow = this.data[key];
+
+      for (let i = 0; i < colsToDelete.length; i++) {
+        if (this.data[key].hasOwnProperty(colsToDelete[i])) {
+          delete tempRow[colsToDelete[i]];
+        }
+      }
+
+      rows.push(this.jsonToArray(tempRow));
+    }
+
+    var doc = new jsPDF();
+    doc.autoTable(colsToDisplay, rows, {
+      margin: { top: 20, horizontal: 10, width: 20 },
+      // columnStyles: {
+      //   0: { columnWidth: 15 }, 1: { columnWidth: 15 }, 2: { columnWidth: 55 }, 3: { columnWidth: 100 }
+      // }
+    });
+    doc.save('Test.pdf');
+  }
+
   // generatePdf() {
   //   var columns = ['UserId', 'Id', 'Title', 'Body'];
-  //   let colSel = ['userId', 'title'];
   //   var rows = [];
-  //   //this.updateSelectedItems(this.data, 'blah');
+
   //   for (let key of Object.keys(this.data)) {
-  //     let tempRow = this.data[key];
-  //     console.log(tempRow);
-  //     if (this.data[key].hasOwnProperty('id')) {
-  //       console.log('hmm');
-  //       delete tempRow.id;
-  //     }
-  //     console.log(tempRow); 
   //     rows.push(this.jsonToArray(this.data[key]));
   //   }
 
@@ -51,26 +92,8 @@ export class PrimeComponent implements OnInit {
   //       0: { columnWidth: 15 }, 1: { columnWidth: 15 }, 2: { columnWidth: 55 }, 3: { columnWidth: 100 }
   //     }
   //   });
-  //   //doc.save('Test.pdf');
+  //   doc.save('Test.pdf');
   // }
-
-  generatePdf() {
-    var columns = ['UserId', 'Id', 'Title', 'Body'];
-    var rows = [];
-    
-    for (let key of Object.keys(this.data)) {
-      rows.push(this.jsonToArray(this.data[key]));
-    }
-
-    var doc = new jsPDF();
-    doc.autoTable(columns, rows, {
-      margin: { top: 20, horizontal: 10, width: 20 },
-      columnStyles: {
-        0: { columnWidth: 15 }, 1: { columnWidth: 15 }, 2: { columnWidth: 55 }, 3: { columnWidth: 100 }
-      }
-    });
-    doc.save('Test.pdf');
-  }
 
   jsonToArray(json) {
     let result = [];
